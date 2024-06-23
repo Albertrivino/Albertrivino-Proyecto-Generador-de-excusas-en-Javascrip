165fe2f5dde0b50bac28806450f8c149be8a38be
Silvia Moraga <110190215+SilviaMoraga@users.noreply.github.com>
2024-06-23 17:43:14 +0000

    [SILVIA MORAGA]
    - app.js: improved legibility and code cleanning

--- src/app.js
+++ src/app.js
@@ -6,13 +6,11 @@ import "./assets/img/tenor.gif";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //con este interconecto el "p = id="excuse"" y el boton de refresh//
   document.querySelector(".btn").addEventListener("click", () => {
-    //querySelecetor se conecta con cualquier elemento del html//
-    document.getElementById("excuse").innerText = MainRandom(); // El geyElementById se conecta con un ID declarado en Html (se borra el numeral en JS)//
+    document.getElementById("excuse").innerText = mainRandom();
   });
 
-  function MainRandom() {
+  function mainRandom() {
     let who = ["The dog", "My grandma", "The mailman", "My bird"];
     let action = ["ate", "peed", "crushed", "broke"];
     let what = ["my homework", "my phone", "the car"];
@@ -23,31 +21,18 @@ window.onload = function() {
       "during my lunch",
       "while I was praying"
     ];
-    function RandomGenerator(list) {
+    function randomGenerator(list) {
       return list[Math.floor(Math.random() * list.length)];
     }
-    // let whoRandom = RandomGenerator(who);
-    // let actionRandom = RandomGenerator(action);
-    // let whatRandom = RandomGenerator(what);
-    // let whenRandom = RandomGenerator(when);
 
-    // console.log(whoRandom);
     return (
-      RandomGenerator(who) +
+      randomGenerator(who) +
       " " +
-      RandomGenerator(action) +
+      randomGenerator(action) +
       " " +
-      RandomGenerator(what) +
+      randomGenerator(what) +
       " " +
-      RandomGenerator(when)
-
-      // who[whoRandom] + //me construye el resultado entre who y whoRandom//
-      // " " +
-      // action[actionRandom] +
-      // " " +
-      // what[whatRandom] +
-      // " " +
-      // when[whenRandom]
+      randomGenerator(when)
     );
   }
 };
